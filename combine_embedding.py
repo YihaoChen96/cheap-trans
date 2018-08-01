d0cf34b70f5c8e6ef81d26d7ac4af388d0d85eed
YihaoChen96 <yc2288@cornell.edu>
2018-08-01 09:00:57 -0500

    combine embedding with arguments

--- combine_embedding.py
+++ combine_embedding.py
@@ -1,25 +1,28 @@
-with open('/home/yihaoc/software/MUSE/dumped/debug/7vjr2dpeh5/vectors-en.txt','r',encoding='utf-8') as en:
-    with open('/home/yihaoc/software/MUSE/dumped/debug/7vjr2dpeh5/vectors-es.txt','r',encoding='utf-8') as es:
-        with open("/home/yihaoc/data/vectors-en-es.txt",'w',encoding='utf-8') as out:
+import sys
+with open(sys.argv[1],'r') as en:
+    with open(sys.argv[2],'r') as es:
+        with open(sys.argv[3],'w') as out:
             counter = 0
-            for en_line in en.eadlines():
+            print("read 1")
+            for en_line in en.readlines():
                 if counter == 0:
                     entry_num, dimension = en_line.split()
                     entry_sum = int(entry_num)+ int(es.readline().split()[0])
-                    out.write(entry_sum+"\t",+dimension)
+                    out.write(str(entry_sum)+"\t"+dimension+"\n")
                     counter+=1
                 else:
                     line = en_line.split()
-                    line[0]="en_"+line[0]
-                    outline = "\t".join(line)+"\n"
+                    line[0]=""+line[0]
+                    outline = " ".join(line)+"\n"
                     out.write(outline)
             counter = 0
-            for es_line in es.eadlines():
+            print("read 2") 
+            for es_line in es.readlines():
                 if counter == 0:
                     counter+=1
                     continue
                 else:
                     line = es_line.split()
-                    line[0] = "es_" + line[0]
-                    outline = "\t".join(line) + "\n"
+                    line[0] = "" + line[0]
+                    outline = " ".join(line) + "\n"
                     out.write(outline)
--- embedding_coverage.py
+++ embedding_coverage.py
@@ -1,19 +1,26 @@
 from gensim.models import KeyedVectors
-word_vectors = KeyedVectors.load_word2vec_format('/shared/preprocessed/yihaoc/word2vec/wiki.ru.vec')
+#word_vectors = KeyedVectors.load_word2vec_format('/shared/preprocessed/yihaoc/word2vec/wiki.ru.vec')
 print("vector read complete")
 outlines = []
-
-with open('/shared/corpora/ner/lorelei-swm-new/rus/column/rus.train','r',encoding='utf-8') as inf:
-    find=0
-    missed=0
-    for line in inf.readlines():
-        word = line.split()[0].strip()
-        if len(word)<2:
-            continue
-        try:
-            lst = word_vectors.most_similar(positive=[str(line)])
-            find += 1
-        except:
-            missed += 1
+with open("/shared/preprocessed/yihaoc/word2vec/wiki.ru.entry",'r') as vec:
+    lines = vec.readlines()
+    with open('/shared/corpora/ner/lorelei-swm-new/rus/column/rus.train.uniq','r') as inf:
+        find=0
+        missed=0
+        for line in inf.readlines():
+            line = line.split()
+            if len(line)<1:
+                continue
+            word = line[0]
+            try:
+                if word in lines:
+                    find += 1
+                else:
+                    missed+=1
+                #print(word)
+            except:
+                missed += 1
+            finally:
+                print("find:"+str(find)+ " missed:" +str(missed))
 
     print("Find:" + str(find)+"  Missed:"+str(missed))
